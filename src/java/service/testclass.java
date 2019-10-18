822895a45eccf2f16c2d7c105ef5c55ebbb3829b
patri <patri@LAPTOP-UVAQRBVN>
2019-10-18 17:47:21 +0200

    First commit testing with testclass 18.10.2019

---
+++ src/java/service/testclass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/**
+ *
+ * @author patri
+ */
+public class testclass {
+    private int data;
+
+    public testclass(int data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "testclass{" + "data=" + data + '}';
+    }
+    
+    
+}
