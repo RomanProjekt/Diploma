263f756b8e85a14d6e5e63e5da92a5e87815f535
patri <patri@LAPTOP-UVAQRBVN>
2019-11-09 15:25:19 +0100

    User Register and Login

--- src/java/infrastructure/benutzerBean.java
+++ src/java/infrastructure/benutzerBean.java
@@ -12,14 +12,28 @@ import javax.faces.bean.SessionScoped;
  *
  * @author patri
  */
-@ManagedBean
-@SessionScoped
+
 public class benutzerBean {
+
+    private int user_id;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String passWd;
+    private String salt;
+    private String role;
+    private String eMail;
 
     /**
      * Creates a new instance of benutzerBean
      */
     public benutzerBean() {
+        
     }
     
+    public Object register() {
+        //in die datenbank schreiben
+        return null;
+    }
+
 }
