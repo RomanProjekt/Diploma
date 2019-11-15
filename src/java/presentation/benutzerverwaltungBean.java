3a68ea5de2df7942ca78bcd438a900f3aebf9de2
Patrick Dopler <patrick.dopler@htlpinkafeld.at>
2019-11-15 20:27:57 +0100

    add package presentation and benutzerverwaltungBean

---
+++ src/java/presentation/benutzerverwaltungBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentation;
+
+/**
+ *
+ * @author dople
+ */
+public class benutzerverwaltungBean {
+
+    /**
+     * Creates a new instance of benutzerverwaltungBean
+     */
+    public benutzerverwaltungBean() {
+    }
+    
+}
