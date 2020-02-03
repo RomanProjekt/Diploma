c792da0dee75e9d67c7860f5f2425480ec374f00
hp <hp@10.0.0.5>
2020-02-03 14:10:19 +0100

    Bearbeitet: emai_passwort, loginBean

--- src/java/presentation/loginBean.java
+++ src/java/presentation/loginBean.java
@@ -38,7 +38,7 @@ public class loginBean {
     private String username;
     
     //Variable Passwort zurücksetzen
-    private Session mailSession;
+//    private Session mailSession;
 
 
     public loginBean() {
@@ -119,14 +119,14 @@ public class loginBean {
         this.username = username;
     }
 
-    public Session getMailSession() {
-        return mailSession;
-    }
-
-    public void setMailSession(Session mailSession) {
-        this.mailSession = mailSession;
-    }
-    
+//    public Session getMailSession() {
+//        return mailSession;
+//    }
+//
+//    public void setMailSession(Session mailSession) {
+//        this.mailSession = mailSession;
+//    }
+//    
     
     
     
@@ -144,7 +144,7 @@ public class loginBean {
             
             //1.Einloggen der AK in den email-server
             //Testen: sender.login("mail.gmx.net", "587", username, password);
-            this.login(smtpHost, smtPort, email_username, email_passwort);
+//            this.login(smtpHost, smtPort, email_username, email_passwort);
             
             
             //2. Senden des Texten an den email-Server
@@ -161,49 +161,49 @@ public class loginBean {
     }
     
     
-    public void login(String smtpHost, String smtpPort, String email_username, String email_passwort) {
-			Properties props = new Properties();
-			props.put("mail.smtp.host", smtpHost);
-			props.put("mail.smtp.socketFactory.port", smtpPort);
-			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-			props.put("mail.smtp.auth", "true");
-			props.put("mail.smtp.port", smtpPort);
-
-			Authenticator auth = new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(email_username, email_passwort);
-				}
-			};
-
-			this.mailSession = Session.getDefaultInstance(props, auth);
-			System.out.println("Eingeloggt.");
-    }
+//    public void login(String smtpHost, String smtpPort, String email_username, String email_passwort) {
+//			Properties props = new Properties();
+//			props.put("mail.smtp.host", smtpHost);
+//			props.put("mail.smtp.socketFactory.port", smtpPort);
+//			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//			props.put("mail.smtp.auth", "true");
+//			props.put("mail.smtp.port", smtpPort);
+//
+//			Authenticator auth = new Authenticator() {
+//				@Override
+//				protected PasswordAuthentication getPasswordAuthentication() {
+//					return new PasswordAuthentication(email_username, email_passwort);
+//				}
+//			};
+//
+//			this.mailSession = Session.getDefaultInstance(props, auth);
+//			System.out.println("Eingeloggt.");
+//    }
     
     
-    public void send(String senderMail, String senderName, String receiverAddresses, String subject, String message)
-				throws MessagingException, IllegalStateException, UnsupportedEncodingException {
-			if (mailSession == null) {
-				throw new IllegalStateException("Du musst dich zuerst einloggen (login()-Methode)");
-			}
-
-			MimeMessage msg = new MimeMessage(mailSession);
-			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
-			msg.addHeader("format", "flowed");
-			msg.addHeader("Content-Transfer-Encoding", "8bit");
-
-			msg.setFrom(new InternetAddress(senderMail, senderName));
-			msg.setReplyTo(InternetAddress.parse(senderMail, false));
-			msg.setSubject(subject, "UTF-8");
-			msg.setText(message, "UTF-8");
-			msg.setSentDate(new Date());
-
-			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverAddresses, false));
-
-			System.out.println("Versende E-Mail...");
-			Transport.send(msg);
-			System.out.println("E-Mail versendet.");
-		}
+//    public void send(String senderMail, String senderName, String receiverAddresses, String subject, String message)
+//				throws MessagingException, IllegalStateException, UnsupportedEncodingException {
+//			if (mailSession == null) {
+//				throw new IllegalStateException("Du musst dich zuerst einloggen (login()-Methode)");
+//			}
+//
+//			MimeMessage msg = new MimeMessage(mailSession);
+//			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+//			msg.addHeader("format", "flowed");
+//			msg.addHeader("Content-Transfer-Encoding", "8bit");
+//
+//			msg.setFrom(new InternetAddress(senderMail, senderName));
+//			msg.setReplyTo(InternetAddress.parse(senderMail, false));
+//			msg.setSubject(subject, "UTF-8");
+//			msg.setText(message, "UTF-8");
+//			msg.setSentDate(new Date());
+//
+//			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverAddresses, false));
+//
+//			System.out.println("Versende E-Mail...");
+//			Transport.send(msg);
+//			System.out.println("E-Mail versendet.");
+//		}
                 
                 
     
